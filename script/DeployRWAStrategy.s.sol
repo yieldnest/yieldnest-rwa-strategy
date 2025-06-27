bbbf0cccfa6febf2a832a1cf43e2306f816c7113
danoctavian <danoctavian91@gmail.com>
2025-06-27 05:50:42 +0300

    add sample script

---
+++ script/DeployRWAStrategy.s.sol
@@ -0,0 +1,5 @@
+import "lib/yieldnest-flex-strategy/script/DeployFlexStrategy.s.sol";
+
+contract DeployRWAStrategy is DeployFlexStrategy {
+    // Additional functionality for DeployRWAStrategy can be added here
+}
