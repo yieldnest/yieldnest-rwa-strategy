98da1b07de57bf7f5cb0c367d28c80ee5b91d451
danoctavian <danoctavian91@gmail.com>
2025-06-27 07:42:53 +0300

    add rwa deploy script

--- script/DeployRWAStrategy.s.sol
+++ script/DeployRWAStrategy.s.sol
@@ -5,6 +5,7 @@ import "lib/yieldnest-flex-strategy/script/DeployFlexStrategy.s.sol";
 import { L1Contracts } from "@yieldnest-vault-script/Contracts.sol";
 import { IContracts } from "@yieldnest-vault-script/Contracts.sol";
 import { IActors } from "@yieldnest-vault-script/Actors.sol";
+import { console } from "forge-std/console.sol";
 
 
 contract MainnetRWAStrategyActors is IActors {
@@ -38,6 +39,33 @@ contract MainnetRWAStrategyActors is IActors {
 
 contract DeployRWAStrategy is DeployFlexStrategy {
 // Additional functionality for DeployRWAStrategy can be added here
+
+    address public constant USDC = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
+    address public constant ynRWAx = 0x01Ba69727E2860b37bc1a2bd56999c1aFb4C15D8;
+
+    function run() public override {
+        setDeploymentParameters(
+            DeploymentParameters({
+                name: "YieldNest USDC Private Credit",
+                symbol_: "ynRWA-USDC-PrivateCredit",
+                accountTokenName: "YieldNest Flex Token",
+                accountTokenSymbol: "YNFLEX",
+                decimals: 6,
+                paused: true,
+                targetApy: 0.1 ether,
+                lowerBound: 0.5 ether,
+                minRewardableAssets: 1000e6,
+                accountingProcessor: accountingProcessor,
+                baseAsset: USDC,
+                allocator: ynRWAx,
+                safe: safe,
+                alwaysComputeTotalAssets: true
+            })
+        );
+        
+        super.run();
+    }
+
 
 
     function _setup() public override {
