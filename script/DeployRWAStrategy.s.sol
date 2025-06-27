ba9b4745a2a4e1bd36d8c6c0b40bfb7692b17404
danoctavian <danoctavian91@gmail.com>
2025-06-27 06:31:58 +0300

    working deploy

--- script/DeployRWAStrategy.s.sol
+++ script/DeployRWAStrategy.s.sol
@@ -1,5 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+
 import "lib/yieldnest-flex-strategy/script/DeployFlexStrategy.s.sol";
 
 contract DeployRWAStrategy is DeployFlexStrategy {
-    // Additional functionality for DeployRWAStrategy can be added here
+// Additional functionality for DeployRWAStrategy can be added here
+
+
+    // function _setup() public override {
+    //     if (block.chainid == 1) {
+    //         minDelay = 1 days;
+    //         MainnetActors _actors = new MainnetActors();
+    //         actors = IActors(_actors);
+    //         contracts = IContracts(new L1Contracts());
+    //     }
+    // }
+
 }
