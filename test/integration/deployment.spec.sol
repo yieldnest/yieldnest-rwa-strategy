6fe05c8eaa5c07d8277ee574724fdaf114179f45
danoctavian <danoctavian91@gmail.com>
2025-06-30 08:51:06 +0300

    edit test

--- test/integration/deployment.spec.sol
+++ test/integration/deployment.spec.sol
@@ -13,27 +13,5 @@ contract FlexStrategyDeployment is BaseIntegrationTest {
         verify.run();
     }
 
-    // function test_upgrade_success() public {
-    //     FlexStrategy newImpl = new FlexStrategy();
-    //     address securityCouncil = MainnetActors(address(deployment.actors())).YnSecurityCouncil();
-    //     UpgradeUtils.timelockUpgrade(
-    //         deployment.timelock(), securityCouncil, address(deployment.strategy()), address(newImpl)
-    //     );
-
-    //     assertEq(address(ProxyUtils.getImplementation(address(deployment.strategy()))), address(newImpl));
-    // }
-
-    // function test_addNewAdmin_success() public {
-    //     address newAdmin = address(0x1234567890123456789012345678901234567890);
-
-    //     vm.startPrank(deployment.actors().ADMIN());
-    //     deployment.strategy().grantRole(deployment.strategy().DEFAULT_ADMIN_ROLE(), newAdmin);
-    //     RolesVerification.verifyRole(
-    //         deployment.strategy(),
-    //         newAdmin,
-    //         deployment.strategy().DEFAULT_ADMIN_ROLE(),
-    //         true,
-    //         "newAdmin has DEFAULT_ADMIN_ROLE"
-    //     );
-    // }
+    // TODO: add test for upgrade
 }
